a36a7b28bd656b049542215ed13c94566acbe402
wpavini <62146804+wpavini@users.noreply.github.com>
2020-04-02 17:04:43 -0300

    incluido requirements do projeto, app_restful como exemplo da aplicação e lista de habilidades

---
+++ habilidades.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+lista_habilidades = ['Python,' 'Java', 'Flask', 'PHP']
+class Habilidades(Resource):
+    def get(self):
+        return lista_habilidades
+
+
+
+
